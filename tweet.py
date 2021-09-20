d0d74ff8a52f54817993abf41a1a2d7d41e6e9c6
Luke Hollenback <8144622+lukehollenback@users.noreply.github.com>
2021-09-20 14:38:53 -0600

    Added ability to delete tweet source files after posting.

--- tweet.py
+++ tweet.py
@@ -15,6 +15,8 @@ arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument("--path", help = "file or directory (random will be chosen) of file to tweet")
 arg_parser.add_argument("--media", help = "file or directory (random will be chosen) of media to attach to tweet")
+arg_parser.add_argument("--delete-content", action = "store_true", help = "delete the tweet's content file after posting")
+arg_parser.add_argument("--delete-media", action = "store_true", help = "delete the tweet's media file after posting")
 
 args = arg_parser.parse_args()
 
@@ -67,3 +69,12 @@ with open(args.path, "r") as file:
     # Print out the id of the new tweet (in case it needs to be piped into another script).
     #
     print(tweet.id)
+
+#
+# Delete files if necessary.
+#
+if args.delete_content:
+    os.remove(args.path)
+    
+if args.media and args.delete_media:
+    os.remove(args.media)
