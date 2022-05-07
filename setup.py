d6829f87b26c7769043c7cd1f32d41d289208887
Luke Hollenback <8144622+lukehollenback@users.noreply.github.com>
2022-05-07 14:43:15 -0600

    Created module's setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='birdcall',
+    version='1.0.0',
+    packages=['birdcall'],
+    url='https://github.com/lukehollenback/birdcall',
+    license='Apache License 2.0',
+    author='Luke Hollenback',
+    author_email='luke.hollenback+birdcall@gmail.com',
+    description='Module for serverless Twitter bots.'
+)
