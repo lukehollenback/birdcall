80eedb51d62fa683924441bcb002fbbcc18634d7
Luke Hollenback <8144622+lukehollenback@users.noreply.github.com>
2022-05-09 16:55:25 -0600

    Fixed module installation issues.

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
+import setuptools
 from setuptools import setup
 
 setup(
     name='birdcall',
     version='1.0.0',
-    packages=['birdcall'],
+    packages=setuptools.find_packages(),
     url='https://github.com/lukehollenback/birdcall',
     license='Apache License 2.0',
     author='Luke Hollenback',
     author_email='luke.hollenback+birdcall@gmail.com',
-    description='Module for serverless Twitter bots.'
+    description='Module for serverless Twitter bots.',
+    install_requires=['python-dotenv', 'tweepy'],
+    python_requires='>=3.8'
 )
